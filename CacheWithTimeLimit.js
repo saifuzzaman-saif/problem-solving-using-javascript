da671464586e18aed8fe3f0ed0d2e6db3c0da138
saifuzzaman <saifuriit46@gmail.com>
2023-07-26 23:34:40 +0600

    Added the solution of LeetCode 2622. Cache With Time Limit.

---
+++ CacheWithTimeLimit.js
@@ -0,0 +1,37 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let data = this.cache.get(key);
+
+    if (data) {
+        clearTimeout(data.timeoutId);
+    }
+
+    let timeoutId = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, {value, timeoutId});
+
+    return data != undefined && data != null;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
